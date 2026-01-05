0dc6fec2df0518f746d0aa4cd12bb8ffe15bc5a3
KiraHuang <hson_kira@kutech.tw>
2026-01-05 15:52:16 +0800

    docs: 更新註解

--- WebApi/medPictureClass.cs
+++ WebApi/medPictureClass.cs
@@ -22,8 +22,14 @@ namespace DB2VM_API
         [JsonPropertyName("fullName")]
         public string 藥名 { get; set; }
 
+        /// <summary>
+        /// 取得藥品圖片檔
+        /// </summary>
+        /// <param name="code">藥碼</param>
+        /// <returns>藥檔圖片物件</returns>
         static public medPictureClass get_pic(string code)
         {
+            // 藥碼取得藥品圖片(中榮藥碼圖片連結)  得到的是JPEG 二進制資料 非Base64
             string url = $"https://www3.vghtc.gov.tw:8443/pharmacyHandbook/API/getImage.jsp?path=pic&code={code}";
             medPictureClass medPicClass = new medPictureClass();
             string json_out = WEBApiGet(url);
@@ -33,11 +39,21 @@ namespace DB2VM_API
             return medPicClass;
         }
 
+        /// <summary>
+        /// 擷取圖檔連結 (包成同步)
+        /// </summary>
+        /// <param name="url">圖檔連結</param>
+        /// <returns>Base64字串</returns>
         static string WEBApiGet(string url)
         {
             return Task.Run(async () => await WEBApiGetAsync(url)).Result;
         }
 
+        /// <summary>
+        /// 擷取圖檔連結 (非同步)
+        /// </summary>
+        /// <param name="url">圖檔連結</param>
+        /// <returns>Base64字串</returns>
         static async Task<string> WEBApiGetAsync(string url)
         {
             string responseBody = "";
