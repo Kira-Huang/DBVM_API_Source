73ce8e6533122c8c81b06d78ebd7f36a70a8de3a
DESKTOP-K4UC0O8\user <Kira740125@outlook.com>
2026-01-08 23:03:55 +0800

    feat: 增加odrers  batch 檔

---
+++ ConsoleApp_OrderList/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_OrderList
+{
+    class Program
+    {
+        static readonly HttpClient _httpClient = new HttpClient
+        {
+            Timeout = TimeSpan.FromSeconds(20)
+        };
+        static string _baseUrl = "http://192.168.5.200:4434";
+
+        static async Task Main(string[] args)
+        {
+            DateTime today = DateTime.Now;
+
+            string startDate = today.ToString("yyyyMMdd") + "000001";
+            string endDate = today.ToString("yyyyMMdd") + "235959";
+
+            string url = $"{_baseUrl}" + $"/dbvm/Orders" +
+                         $"?startDate={startDate}&endDate={endDate}";
+
+            try
+            {
+                var response = await _httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string json = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine($"API: {url} 呼叫成功");
+                Console.WriteLine(json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"API: {url}: {ex.Message}");
+            }
+        }
+    }
+}
