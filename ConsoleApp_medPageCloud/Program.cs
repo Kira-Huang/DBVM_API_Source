9a036c8366d976208d3006910e15dd0ac273c7b5
KiraHuang <hson_kira@kutech.tw>
2026-01-22 10:43:29 +0800

    fix: batch檔調整

--- ConsoleApp_medPageCloud/Program.cs
+++ ConsoleApp_medPageCloud/Program.cs
@@ -8,12 +8,15 @@ namespace ConsoleApp_medPageCloud
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("藥檔更新開始");
-            string url = "http://127.0.0.1:4433/api/BBCM";
-            string json = Basic.Net.WEBApiGet(url);
-            Console.WriteLine("藥檔更新結束");
+            string url = string.Empty;
+            string json = string.Empty;
+
+            //Console.WriteLine("藥檔更新開始");
+            //url = "http://192.168.5.200:4434/api/BBCM";
+            //json = Basic.Net.WEBApiGet(url);
+            //Console.WriteLine("藥檔更新結束");
             Console.WriteLine("藥品圖片更新開始");
-            url = "http://127.0.0.1:4433/api/med_pic";
+            url = "http://192.168.5.200:4434/api/med_pic";
             json = Basic.Net.WEBApiGet(url);
             Console.WriteLine("藥品圖片更新結束");
         }
